fead1bc197bd07d19c9e2b9fc01db5d1dade66aa
shahud <shahudmalik5@gmail.com>
2022-04-06 03:15:29 +0500

    done alignment

--- src/Components/aliceHeader.js
+++ src/Components/aliceHeader.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
+import React, { useState } from 'react'
 import threeDots from '../assets/threeDots.png'
 import portfolio from '../assets/portfolio.png'
 import pic from '../assets/pic.png'
@@ -8,10 +8,30 @@ import convert from '../assets/convert.png'
 import transfer from '../assets/transfer.png'
 import withdraw from '../assets/withdraw.png'
 import './aliceHeader.css'
+import depositB from '../assets/depositB.png'
+import convertW from '../assets/converW.png'
+import transferW from '../assets/transferW.png'
+import withdrawW from '../assets/withdrawW.png'
+
 
 function AliceHeader() {
+
+  const [one, setOne] = useState(0);
+  const [two, setTwo] = useState(0);
+  const [three, setThree] = useState(0);
+  const [four, setFour] = useState(0);
+  console.log(one);
   return (
     <div className='page'>
+
+      {/* <div className='logout'>
+        <div className='sYel'><img src={threeDots} /></div>
+        <div>
+          <p>Setting</p>
+          <p>Logout</p>
+        </div>
+      </div> */}
+
       <div className='top'>
         <div className='bYel'>GBFO5...DQKW7</div>
         <div className='sYel'><img src={threeDots} /></div>
@@ -62,31 +82,33 @@ function AliceHeader() {
             <div className='portfolio'>
               <img src={portfolio} height={30} width={30} />
               <p>$7,464,000.00</p>
-              <p style={{ color: '#EB5721',fontSize:'100%' }}>+3.6%</p>
+              <p style={{ color: '#d39d09', fontSize: '100%' }}>+3.6%</p>
             </div>
           </div>
 
           <div className='dDiv'>
-            <div className='wDiv' style={{ backgroundColor:'#313147'}}>
-              <img src={deposit} />
-              <p style={{color:'white'}}>Deposit</p>
+            <div className='wDiv' onMouseOver={() => setOne(1)} onMouseOut={() => setOne(0)} >
+              <img src={one === 0 ? depositB : deposit} />
+              <p>Deposit</p>
             </div>
-            <div style={{width:'10%'}}></div>
-            <div className='wDiv'>
-              <img src={transfer} />
+            <div style={{ width: '10%' }}></div>
+            <div className='wDiv' onMouseOver={() => setTwo(1)} onMouseOut={() => setTwo(0)}>
+              <img src={two === 0 ? transfer : transferW} />
               <p>Transfer</p>
             </div>
           </div>
 
+          <div style={{ height: '13px' }}></div>
+
           <div className='dDiv'>
-            <div className='wDiv'>
-              <img src={withdraw}/>
+            <div className='wDiv' onMouseOver={() => setThree(1)} onMouseOut={() => setThree(0)}>
+              <img src={three === 0 ? withdraw : withdrawW} />
               <p>Withdraw</p>
             </div>
             <div style={{ width: '10%' }}></div>
-            <div className='wDiv'>
-              <img src={convert} />
-              <p>convert</p>
+            <div className='wDiv' onMouseOver={() => setFour(1)} onMouseOut={() => setFour(0)}>
+              <img src={four === 0 ? convert : convertW} />
+              <p>Convert</p>
             </div>
           </div>
 
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import AliceHeader from '../Components/aliceHeader'
 function Home() {
   return (
-   <div style={{padding:'5% 7% 7% 7%'}}>
+   <div>
 <AliceHeader/>
    </div>
   )
